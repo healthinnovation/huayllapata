76b2070eafbf77e6a26290261770ac1dd589f5eb
antony <antony.barja8@gmail.com>
2023-06-13 18:34:21 -0500

    update

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 1060587,
+    "boundingBox": {
+        "lx": 857097.68,
+        "ly": 8504837.45,
+        "lz": 3610.13,
+        "ux": 858775.6100000016,
+        "uy": 8506515.38,
+        "uz": 5288.060000001565
+    },
+    "tightBoundingBox": {
+        "lx": 857097.68,
+        "ly": 8504837.45,
+        "lz": 3610.13,
+        "ux": 858712.29,
+        "uy": 8506515.38,
+        "uz": 3992.57
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 14.531299591064454,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
